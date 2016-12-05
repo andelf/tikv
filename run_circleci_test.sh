ef58ed0f9d3d6f8796998a1d592efa0cb06d19c6
andelf <andelf@gmail.com>
2016-12-06 00:57:26 +0800

    ci: rebalance split

--- run_circleci_test.sh
+++ run_circleci_test.sh
@@ -2,18 +2,25 @@
 
 case $CIRCLE_NODE_INDEX in
     0)
+        echo Test: format, misc
         make format && git diff-index --quiet HEAD -- || \
                 (echo please make format before creating a pr!; exit 1)
         cargo test --features "default" --bench benches -- --nocapture
-        cargo test --features "default" -- --skip coprocessor --skip raft --skip storage
+        cargo test --features "default" -- \
+              --skip raftstore
         ;;
     1)
-        cargo test --features "default" -- coprocessor
+        cargo test --features "default" -- \
+              raftstore::test_multi
         ;;
     2)
-        cargo test --features "default" -- raftstore
+        cargo test --features "default" -- \
+              raftstore::store
         ;;
     3)
-        cargo test --features "default" -- storage
+        cargo test --features "default" -- \
+              raftstore \
+              --skip raftstore::test_multi \
+              --skip raftstore::store
         ;;
 esac
