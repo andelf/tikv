38f0d112d676a23addf6b0a3df6bf8591bc8665c
andelf <andelf@gmail.com>
2016-12-06 11:23:17 +0800

    ci: fix typo

--- run_circleci_test.sh
+++ run_circleci_test.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-
 # Job 1
-if test $(( 0 % $CIRCLE_NODE_TATAL)) -eq $CIRCLE_NODE_INDEX; then
+if test $(( 0 % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX; then
     make format && git diff-index --quiet HEAD -- || \
             (echo please make format before creating a pr!; exit 1)
     cargo test --features "default" --bench benches -- --nocapture
@@ -10,21 +9,21 @@ if test $(( 0 % $CIRCLE_NODE_TATAL)) -eq $CIRCLE_NODE_INDEX; then
 fi
 
 # Job 2
-if test $(( 1 % $CIRCLE_NODE_TATAL)) -eq $CIRCLE_NODE_INDEX; then
-    cargo test --features "default" -- raftstore::test_multi
-fi
-
-# Job 3
-if test $(( 2 % $CIRCLE_NODE_TATAL)) -eq $CIRCLE_NODE_INDEX; then
-    cargo test --features "default" -- raftstore::test_single
-    cargo test --features "default" -- raftstore::test_tombstone
-fi
-
-# Job 4
-if test $(( 3 % $CIRCLE_NODE_TATAL)) -eq $CIRCLE_NODE_INDEX; then
+if test $(( 1 % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX; then
     cargo test --features "default" -- \
           --skip raftstore::test_single \
           --skip raftstore::test_multi \
           --skip raftstore::test_tombstone \
           raftstore
+fi
+
+# Job 3
+if test $(( 2 % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX; then
+    cargo test --features "default" -- raftstore::test_single
+    cargo test --features "default" -- raftstore::test_tombstone
+fi
+
+# Job 4
+if test $(( 3 % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX; then
+    cargo test --features "default" -- raftstore::test_multi
 fi
