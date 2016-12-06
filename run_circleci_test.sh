cd5bf0b5822692e1cac91b0e876568de77dbda0f
andelf <andelf@gmail.com>
2016-12-06 13:12:15 +0800

    ci: rebalance split

--- run_circleci_test.sh
+++ run_circleci_test.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# Job 1
+set -x
+
+if test -z "$CIRCLE_NODE_TOTAL"; then
+    echo must be run under CircleCI!
+    exit 1
+fi
+
+# Job 0
 if test $(( 0 % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX; then
     make format && git diff-index --quiet HEAD -- || \
             (echo please make format before creating a pr!; exit 1)
@@ -8,8 +15,21 @@ if test $(( 0 % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX; then
     cargo test --features "default" -- --skip raftstore
 fi
 
-# Job 2
+# Job 1
 if test $(( 1 % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX; then
+    cargo test --features "default" -- raftstore::test_multi
+fi
+
+# Job 2
+if test $(( 2 % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX; then
+    time cargo test --features "default" -- raftstore::test_single
+    time cargo test --features "default" -- raftstore::test_tombstone
+    time cargo test --features "default" -- raftstore::test_compact_log
+    time cargo test --features "default" -- raftstore::test_snap
+fi
+
+# Job 3
+if test $(( 3 % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX; then
     cargo test --features "default" -- \
           --skip raftstore::test_single \
           --skip raftstore::test_multi \
@@ -17,17 +37,4 @@ if test $(( 1 % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX; then
           --skip raftstore::test_compact_log \
           --skip raftstore::test_snap \
           raftstore
-fi
-
-# Job 3
-if test $(( 2 % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX; then
-    cargo test --features "default" -- raftstore::test_single
-    cargo test --features "default" -- raftstore::test_tombstone
-    cargo test --features "default" -- raftstore::test_compact_log
-    cargo test --features "default" -- raftstore::test_snap
-fi
-
-# Job 4
-if test $(( 3 % $CIRCLE_NODE_TOTAL)) -eq $CIRCLE_NODE_INDEX; then
-    cargo test --features "default" -- raftstore::test_multi
 fi
