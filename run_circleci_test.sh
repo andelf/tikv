32bfddae52f83244dfbd22af968abdeece8f8d36
andelf <andelf@gmail.com>
2016-12-06 11:13:57 +0800

    ci: rebalance split

--- run_circleci_test.sh
+++ run_circleci_test.sh
@@ -1,34 +1,30 @@
 #!/bin/bash
 
-case $CIRCLE_NODE_INDEX in
-    0)
-        echo Test: format, misc
-        make format && git diff-index --quiet HEAD -- || \
-                (echo please make format before creating a pr!; exit 1)
-        cargo test --features "default" --bench benches -- --nocapture
-        cargo test --features "default" -- \
-              --skip raftstore
-        ;;
-    1)
-        cargo test --features "default" -- \
-              raftstore::test_multi
-        ;;
-    2)
-        cargo test --features "default" -- \
-              raftstore::store \
-              raftstore::test_split_region \
-              raftstore::test_snap \
-              raftstore::coprocessor \
-              raftstore::test_compact
-        ;;
-    3)
-        cargo test --features "default" -- \
-              --skip raftstore::test_multi \
-              --skip raftstore::store \
-              --skip raftstore::test_split_region \
-              --skip raftstore::test_snap \
-              --skip raftstore::coprocessor \
-              --skip raftstore::test_compact \
-              raftstore
-        ;;
-esac
+
+# Job 1
+if test $(( 0 % $CIRCLE_NODE_TATAL)) -eq $CIRCLE_NODE_INDEX; then
+    make format && git diff-index --quiet HEAD -- || \
+            (echo please make format before creating a pr!; exit 1)
+    cargo test --features "default" --bench benches -- --nocapture
+    cargo test --features "default" -- --skip raftstore
+fi
+
+# Job 2
+if test $(( 1 % $CIRCLE_NODE_TATAL)) -eq $CIRCLE_NODE_INDEX; then
+    cargo test --features "default" -- raftstore::test_multi
+fi
+
+# Job 3
+if test $(( 2 % $CIRCLE_NODE_TATAL)) -eq $CIRCLE_NODE_INDEX; then
+    cargo test --features "default" -- raftstore::test_single
+    cargo test --features "default" -- raftstore::test_tombstone
+fi
+
+# Job 4
+if test $(( 3 % $CIRCLE_NODE_TATAL)) -eq $CIRCLE_NODE_INDEX; then
+    cargo test --features "default" -- \
+          --skip raftstore::test_single \
+          --skip raftstore::test_multi \
+          --skip raftstore::test_tombstone \
+          raftstore
+fi
